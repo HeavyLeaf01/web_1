e961367acf9870892881e20b4febb8912dddbf9b
HeavyLeaf <gabigoolgabriell@gmail.com>
2026-09-11 10:50:34 -0300

    Update

--- atividade_aula6/main.js
+++ atividade_aula6/main.js
@@ -8,5 +8,21 @@ function VerificaIdade() {
 }
 
 function VerificaNumero(){
-    
+    let Numero = Number($("#Numero").val());
+    if (Numero < 0) {
+        $(".Resultado2").text("O número é negativo.").css("color", "green");
+    } else if (Numero === 0) {
+        $(".Resultado2").text("O número é 0.").css("color", "red");
+    } else {
+        $(".Resultado2").text("O número é positivo.").css("color", "blue");
+    }
+}
+
+function VerificaNumero2(){
+    let Numero2 = Number($("#Numero2").val());
+    if (Numero2 % 2 === 0) {
+        $(".Resultado3").text("O número é par").css("color", "green");
+    } else {
+        $(".Resultado3").text("O número é impar").css("color", "red");
+    }
 }
